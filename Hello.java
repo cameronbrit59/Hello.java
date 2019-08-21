4b47e1fc5baca54b8537bd9f8f9f3956646dd850
cameronbrit59 <54374719+cameronbrit59@users.noreply.github.com>
2019-08-21 15:03:23 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,24 @@
+/*
+Java SE JDK 8u221
+Intro to Programming Lab 1
+Wayne Britton
+8/21/19
+CMSC 255 002
+*/
+
+public class Hello {
+ public static void printHeading() {
+  System.out.println("Wayne");
+  System.out.println("Java SE JDK 8u221");
+  System.out.println("Intro to Programming Lab 1");
+  System.out.println("8");
+  System.out.println("CMSC 255 002");
+  System.out.println();
+ }
+ 
+ 
+ public static void main(String [] args) {
+	 printHeading();
+	 System.out.println("Hello");
+ }
+} 
